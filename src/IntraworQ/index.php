00ddeece869af0266e22e32c71797cd3acba4c0e
rbgazdda <rg@intraworq.com>
2015-02-06 15:32:58 +0100

    message, time line, exceptions

--- src/IntraworQ/index.php
+++ src/IntraworQ/index.php
@@ -3,11 +3,11 @@
 require __DIR__ . '/../../vendor/autoload.php';
 require 'config.php';
 
-const DEBUGBAR_PATH = '/../../vendor/maximebf/debugbar/src/DebugBar/Resources';
+	const DEBUGBAR_PATH = '/../../vendor/maximebf/debugbar/src/DebugBar/Resources';
 
 use DebugBar\StandardDebugBar;
 
-$debugBar = new DebugBar\StandardDebugBar();
+$debugBar = $debugbar = new DebugBar\StandardDebugBar();
 
 $app = new \Slim\Slim([
 	'view' => new \Slim\Views\Smarty(),
@@ -22,17 +22,28 @@ $view->parserExtensions = array(
 	__DIR__ . '/vendor/slim/views/Slim/Views/SmartyPlugins',
 );
 
-$app->container->singleton('log', function () use($config) {
+$app->container->singleton('log',
+	function () use($config) {
 	Logger::configure($config['logger']);
 	$log = Logger::getLogger('planq');
-	
+
 	return $log;
 });
+
 $app->container->singleton('message', function () use($debugBar) {
-	return $debugBar["messages"];
+	return $debugBar->getCollector('messages');
 });
 
-$app->container->singleton('db', function () use($config) {
+$app->container->singleton('time', function () use($debugBar) {
+	return $debugBar->getCollector('time');
+});
+
+$app->container->singleton('exceptions', function () use($debugBar) {
+	return $debugBar->getCollector('exceptions');
+});
+
+$app->container->singleton('db',
+	function () use($config) {
 	$pdo = new PDO($config['pdo']['dsn'], $config['pdo']['username'], $config['pdo']['password'], $config['pdo']['options']);
 	return $pdo;
 });
@@ -48,9 +59,10 @@ $app->get('/',
 	$app->log->debug("/ route");
 	$app->render('index.tpl', ['debugbarRenderer' => $app->debugBar->getJavascriptRenderer(DEBUGBAR_PATH)]);
 });
-$app->get('/notes',	function () use($app) {
-	/** sample mesages to debugbar log4pp tab **/
-    $app->log->debug("/notes route debug");
+$app->get('/notes',
+	function () use($app) {
+	/** sample mesages to debugbar log4pp tab * */
+	$app->log->debug("/notes route debug");
 	$app->log->error("error");
 	$app->log->fatal("fatal error ");
 	$app->log->warn("warning");
@@ -65,6 +77,24 @@ $app->get('/notes',	function () use($app) {
 $app->get('/message/(:news)',
 	function ($news = 'Wiadomość') use($app) {
 	$app->message->addMessage($news);
+	$app->time->startMeasure('longop', 'Start message');
+	sleep(2);
+	$app->time->stopMeasure('longop');
+	$app->time->measure('In function sleep', function() {
+		sleep(2);
+	});
+	$app->render('index.tpl', ['debugbarRenderer' => $app->debugBar->getJavascriptRenderer(DEBUGBAR_PATH)]);
+});
+
+$app->get('/exceptions',
+	function () use($app) {
+	try {
+		$t = 5 / 0;
+	} catch (Exception $e) {
+		$app->exceptions->addException($e);
+	}
+
+
 	$app->render('index.tpl', ['debugbarRenderer' => $app->debugBar->getJavascriptRenderer(DEBUGBAR_PATH)]);
 });
 $app->run();
