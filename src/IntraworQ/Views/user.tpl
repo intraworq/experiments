0bac23cd12be900f70954cd079bf45dcbb78da0f
mpiekarczyk <michal.piekarczyk@anglobiznes.pl>
2015-03-04 13:20:17 +0100

    Respect\Validation and simple examples

--- src/IntraworQ/Views/user.tpl
+++ src/IntraworQ/Views/user.tpl
@@ -3,8 +3,9 @@
 
 {block name='body'} 
 <h3>Create new user</h3>
-<form method="post" name='user' id='form' action="/user">
-	<input type="text" name='name' id='name' placeholder="Insert name" />
+<form method="POST" name='user' id='form' action="/user">
+	<input type="text" name='name' id='name' placeholder="Insert name" value="{$user->getName()}" autocomplete="off"/><br/>
+	<input type="text" name='firstName' id='firstName' placeholder="Insert firstName" value="{$user->getFirstName()}" autocomplete="off"/><br/>
 	<input type="submit" value="Submit" />
 </form>
 
--- src/IntraworQ/app.php
+++ src/IntraworQ/app.php
@@ -1,10 +1,11 @@
-<?php 
+<?php
 
 require 'config.php';
 
 use DebugBar\StandardDebugBar;
 use IntraworQ\Models;
 use IntraworQ\Library;
+use Respect\Validation\Validator as v;
 
 $builder = new \DI\ContainerBuilder();
 // $builder->addDefinitions('injections.php');
@@ -19,7 +20,7 @@ $view->parserCacheDirectory = __DIR__ . '/tmp/cache';
 $view->parserExtensions = array(
 	__DIR__ . '/vendor/slim/views/Slim/Views/SmartyPlugins',
 	__DIR__ . '/vendor/smarty-gettext/smarty-gettext'
-	);
+);
 
 $app->container->singleton('db', function () use($config) {
 	$pdo = new PDO($config['pdo']['dsn'], $config['pdo']['username'], $config['pdo']['password'], $config['pdo']['options']);
@@ -38,14 +39,14 @@ $app->container->singleton('debugbar_path', function() {
 
 
 $app->container->singleton('debugBar', function () use($app, $config){
-	$pdo = new \DebugBar\DataCollector\PDO\TraceablePDO($app->db);	
+	$pdo = new \DebugBar\DataCollector\PDO\TraceablePDO($app->db);
 	$debugBar = new DebugBar\StandardDebugBar();
 	$debugBar->addCollector(new \DebugBar\DataCollector\PDO\PDOCollector($pdo));
-	
+
 	return $debugBar;
 });
 $app->container->singleton('faker', function () use($config){
-	$faker = Faker\Factory::create();	
+	$faker = Faker\Factory::create();
 	return $faker;
 });
 
@@ -76,19 +77,34 @@ $app->get('/greet/:name', function($name) use($app) {
 });
 
 $app->post('/user', function() use($app) {
-	$payload = $app->request->post('name');
-	$app->log->info("POST: {$payload} created");
+	$name	   = $app->request->post('name');
+	$firstName = $app->request->post('firstName');
+
+	$user = new \IntraworQ\Models\User($name, $firstName);
+	$userValidator = v::attribute(
+		'name', v::string()->length(1, 30),
+		'firstName', v::string()->length(1, 30)
+	);
+
+	if ($userValidator->validate($user)) {
+		$app->log->info("POST: {$name} created");
+	}
+	else {
+		$app->log->error("USER validation FAIL: " . $userValidator->reportError($user));
+	}
+
 	if($app->request->isAjax()) {
 		$app->log->info('got AJAX request');
 		$a = ['user' => $payload . ' created'];
 		$app->response->write(json_encode($a));
 	} else {
-		$app->response->write($payload . ' created');
+		$app->render('user.tpl', ['user' => $user, 'debugbarRenderer' => $app->debugBar->getJavascriptRenderer($app->debugbar_path)]);
 	}
 });
 
-$app->get('/user', function() use($app) {
-	$app->render('user.tpl', ['user' => new \IntraworQ\Models\User($app->faker->lastName, $app->faker->firstNameMale), 'debugbarRenderer'=>$app->debugBar->getJavascriptRenderer($app->debugbar_path)]);
+$app->get('/user',	function() use($app) {
+	$user = new \IntraworQ\Models\User($app->faker->lastName, $app->faker->firstNameMale);
+	$app->render('user.tpl', ['user' => $user, 'debugbarRenderer' => $app->debugBar->getJavascriptRenderer($app->debugbar_path)]);
 });
 
 $app->get('/user_ajax', function() use($app) {
@@ -120,7 +136,7 @@ $app->get('/long', function() use($app) {
 });
 $app->get('/notes',	function () use($app) {
 	/** sample mesages to debugbar log4pp tab **/
-    $app->log->debug("/notes route debug");
+	$app->log->debug("/notes route debug");
 	$app->log->error("error");
 	$app->log->fatal("fatal error ");
 	$app->log->warn("warning");
@@ -136,7 +152,7 @@ $app->get('/notes',	function () use($app) {
 
 $app->get('/userList',	function () use($app) {
 	for($i=0;$i<=10;$i++) {
-		$users[] = new \IntraworQ\Models\User($app->faker->lastName, $app->faker->firstNameMale,$app->faker->text);	
+		$users[] = new \IntraworQ\Models\User($app->faker->lastName, $app->faker->firstNameMale,$app->faker->text);
 	}
 	$app->render('userList.tpl', ['users' => $users, 'debugbarRenderer'=>$app->debugBar->getJavascriptRenderer($app->debugbar_path)]);
 });
